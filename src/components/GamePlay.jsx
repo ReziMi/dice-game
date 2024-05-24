5dc4bacc3ad9fc2b3ea3f9a410646fdbe96bf7b9
ReziMi <revaz.mikadze92@gmail.com>
2024-05-25 01:19:55 +0400

    NumberSelector and TotalScore jsx files added and are still in wop

--- src/components/GamePlay.jsx
+++ src/components/GamePlay.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import TotalScore from './TotalScore';
+import NumberSelector from './NumberSelector';
 
 const GamePlay = () => {
-  return (
-    <div>GamePlay</div>
-  )
-}
+  return <main>
+    <TotalScore/>
+    <NumberSelector/>
+  </main>
+
+};
 
 export default GamePlay
